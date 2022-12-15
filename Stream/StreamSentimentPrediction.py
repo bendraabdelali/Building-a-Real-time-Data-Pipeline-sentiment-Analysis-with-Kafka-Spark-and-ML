7dd0b47eb2f0735e183db2578cba1c74e73868c7
bendraabdelali <abdelalibendra123@gmail.com>
2022-12-15 10:05:25 +0100

    Reorganizing the project

--- Stream/StreamSentimentPrediction.py
+++ Stream/StreamSentimentPrediction.py
@@ -1,6 +1,6 @@
 # Import libraries
 from pyspark.ml.feature import HashingTF, Tokenizer, StopWordsRemover, CountVectorizer, VectorAssembler
-from pyspark.ml.classification import LinearSVC
+from pyspark.ml.classification import LogisticRegression    
 from pyspark.ml import Pipeline, PipelineModel
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
@@ -23,7 +23,7 @@ df = spark \
      .format("kafka") \
      .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
      .option("subscribe", kafka_topic_name) \
-     .option("startingOffsets", "earliest") \
+     .option("startingOffsets", "latest") \
      .load()\
      .selectExpr("CAST(value AS STRING)") 
 
@@ -40,7 +40,7 @@ df.printSchema()
 df = df.select("SentimentText")
 df.printSchema()
 # Load a pipeline model
-model = PipelineModel.load('./ML/SentimentAnalysis')
+model = PipelineModel.load('SentimentAnalysis')
 
 # Use the model to make predictions on streaming data
 predictions = model.transform(df)
@@ -48,18 +48,11 @@ predictions.printSchema()
 predictionFinal =  predictions.select(
                                 "SentimentText", "prediction")
 predictionFinal.printSchema()
-# Output streaming predictions to the console
-predictionFinal = predictionFinal.select( struct('SentimentText', 'prediction').alias('value'))
-predictionFinal.printSchema()
-# predictionFinal.writeStream.format('console').outputMode('append').start().awaitTermination()
-
-predictionFinal.writeStream.format('csv')\
-           .option("checkpointLocation", "/tmp/pyspark6/")\
-           .option('path', '/output').start()
-# Write the streaming data to a Kafka topic with security options
+predictionFinal.writeStream.format('console').outputMode('append').start().awaitTermination()
 
 # predictionFinal.selectExpr("CAST(value AS STRING)") \
 # .writeStream.format('kafka')\
-# .option('kafka.bootstrap.servers', "kafkas:9092")\
+# .option('kafka.bootstrap.servers', "kafka:9092")\
 # .option("checkpointLocation", "/tmp/pyspark6/")\
-# .option('topic', 'topicB').start()
+# .option('topic', 'topicB').start().awaitTermination()
+
